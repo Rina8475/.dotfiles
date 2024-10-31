1c91d87e2673f1b86fe9d64d06a274c030910ad9
Rina <tokiriasu@gmail.com>
2024-10-31 22:06:42 +0800

    update the logic of install script.

--- install.sh
+++ install.sh
@@ -1,32 +1,29 @@
 #! /usr/bin/env bash
 
-apt_source="# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
-
-# 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
-deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-# deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-
-# 预发布软件源，不建议启用
-# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
-# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse"
-
 # This scripts is used to configure the system after the installation of the OS.
-# This scripts is only for Ubuntu 22.04.
+# This scripts is only for Ubuntu 20.04 and 22.04
 
 echo "This script is sponsered by riann-riu"
 
 os=$(lsb_release -is)
 version=$(lsb_release -rs)
-if [[ $os != "Ubuntu" || $version != "22.04" ]]; then
-    echo "This scripts is only for Ubuntu 22.04."
+if [[ $os != "Ubuntu" ]]; then
+    echo "This scripts is only for Ubuntu."
     exit 1
 fi
+
+case $version in
+    20.04)
+        apt_source=$(cat Ubunut-20.source)
+        ;;
+    22.04)
+        apt_source=$(cat Ubunut-22.source)
+        ;;
+    *)
+        echo "This scripts is only for Ubuntu 20.04 and 22.04."
+        exit 1
+        ;;
+esac
 
 # change the source of apt
 echo "change the source of apt..."
@@ -38,6 +35,10 @@ sudo apt update
 # install esential tools
 echo "install some tools..."
 sudo apt install -y vim git curl wget gcc make build-essential gdb tmux
+
+# set the proxy of git
+git config --global http.proxy http://10.21.245.179:7890
+git config --global https.proxy https://10.21.245.179:7890
 
 # install oh-my-zsh
 echo "install zsh..."
@@ -58,16 +59,30 @@ sudo apt install -y bat
 # configure tmux
 git clone https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin
 
-# clone the dotfiles(.zshrc, .tmux.conf)
+# configure vim
+echo "configure vim..."
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# clone the dotfiles(.zshrc, .tmux.conf, vimrc)
 cd ~
 git clone https://github.com/Rina8475/dotfiles.git
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/vimrc ~/.vim_runtime/my_configs.vim
 source ~/.zshrc
 
 # install other useful tools
 sudo apt install -y tldr shellcheck
 tldr --update  #! this command may fail, but it's ok, user can update later
+
+# install golang version v1.23.2
+echo "install golang..."
+cd ~
+wget https://dl.google.com/go/go1.23.2.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+source ~/.zshrc
 
 # configure git
 echo "configure git..."
@@ -81,9 +96,4 @@ git config --global color.status auto
 git config --global color.grep auto
 git config --global alias.lol "log --graph --oneline --decorate --color --all"
 
-# configure vim
-echo "configure vim..."
-curl -sLf https://spacevim.org/install.sh | bash
-
 echo "Done!"
-
